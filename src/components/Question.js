f5f7f82802393a83a4ffb2a34eccc9f09344b0ec
nathalia <nathaliatroina@gmail.com>
2022-02-05 19:59:21 -0500

    commit tree 176f30e9e920883b4eb9201f9e70a93fdc31fbec

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  // add useEffect code 
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return;
+    }
+
+    const timerId = setTimeout(() => {
+      setTimeRemaining((timeRemaining) => timeRemaining -1);
+    }, 1000);
+
+
+    return function () {
+      clearTimeout(timerId);
+    };
+  }, [timeRemaining, onAnswered]);
+
+
+
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -24,7 +44,7 @@ function Question({ question, onAnswered }) {
           </button>
         );
       })}
-      <h5>{timeRemaining} seconds remaining</h5>
+      <h5>{timeRemaining} seconds remaining </h5>
     </>
   );
 }
